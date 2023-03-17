cb001dd8a091a1d42532263febc4dd94aea6351c
Prajaktafeb22 <prajaktabhandare@hotmail.com>
2023-03-16 18:09:30 -0700

    My new changes

--- Assignment1/src/main/java/org/techArk/utils/Constants.java
+++ Assignment1/src/main/java/org/techArk/utils/Constants.java
@@ -4,6 +4,7 @@ import java.io.File;
 
 public class Constants {
 
+	// Added Comment
 	public final static String PROJECT_HOME = System.getProperty("user.dir");
 	public final static String MAIN_RESOURCES = PROJECT_HOME + File.separator + "src" + File.separator + "main" + File.separator + "resources";
 	public static final String JSON_FILE_PATH = "C:\\Users\\DECOMM\\eclipse-workspace\\Assignment1\\src\\test\\resources\\jsonfile\\jsondata.json";
