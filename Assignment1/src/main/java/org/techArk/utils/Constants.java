f7d25ea25f1592e92aa7fb8db389f52c8b8e5f8b
Prajaktafeb22 <prajaktabhandare@hotmail.com>
2023-03-20 08:27:04 -0700

    My Second change

--- Assignment1/src/main/java/org/techArk/utils/Constants.java
+++ Assignment1/src/main/java/org/techArk/utils/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
 	public final static String PROJECT_HOME = System.getProperty("user.dir");
 	public final static String MAIN_RESOURCES = PROJECT_HOME + File.separator + "src" + File.separator + "main" + File.separator + "resources";
 	public static final String JSON_FILE_PATH = "C:\\Users\\DECOMM\\eclipse-workspace\\Assignment1\\src\\test\\resources\\jsonfile\\jsondata.json";
-	public final static String ENVIRONMENT_DETAILS_PROPERTIES_FILE = MAIN_RESOURCES + File.separator + "EnvironmentDetails" + File.separator + "BaseUri_User_Pass.properties";
+	public final static String pENVIRONMENT_DETAILS_PROPERTIES_FILE = MAIN_RESOURCES + File.separator + "EnvironmentDetails" + File.separator + "BaseUri_User_Pass.properties";
 
 
 
