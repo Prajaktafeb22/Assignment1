8153fab227e2d7dbdca232dabf2e032c05ab6837
Prajaktafeb22 <prajaktabhandare@hotmail.com>
2023-03-20 08:39:49 -0700

    Hello this is my comment

--- Assignment1/src/main/java/org/techArk/utils/Constants.java
+++ Assignment1/src/main/java/org/techArk/utils/Constants.java
@@ -4,11 +4,11 @@ import java.io.File;
 
 public class Constants {
 
-	// Added Comment
+	// added extra new comment
 	public final static String PROJECT_HOME = System.getProperty("user.dir");
 	public final static String MAIN_RESOURCES = PROJECT_HOME + File.separator + "src" + File.separator + "main" + File.separator + "resources";
 	public static final String JSON_FILE_PATH = "C:\\Users\\DECOMM\\eclipse-workspace\\Assignment1\\src\\test\\resources\\jsonfile\\jsondata.json";
-	public final static String pENVIRONMENT_DETAILS_PROPERTIES_FILE = MAIN_RESOURCES + File.separator + "EnvironmentDetails" + File.separator + "BaseUri_User_Pass.properties";
+	public final static String ENVIRONMENT_DETAILS_PROPERTIES_FILE = MAIN_RESOURCES + File.separator + "EnvironmentDetails" + File.separator + "BaseUri_User_Pass.properties";
 
 
 
